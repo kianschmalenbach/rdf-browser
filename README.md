1db724846c0f2c121457868925e5a5100ebf92dc
Kian Schmalenbach <kian.schmalenbach@fau.de>
2019-09-19 13:25:51 +0200

    added modification of HTTP request accept header

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # RDF Browser
 
+## Getting Started
 To get started, install the WebExtension on your browser, e.g.:
 
 https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Temporary_Installation_in_Firefox
 https://developer.chrome.com/extensions/getstarted (not tested)
+
+### Short Manual for Firefox 67+:
+  - Go to [about:debugging](about:debugging)
+  - Click *This Firefox*
+  - Click *Load Temporary Add-On*
+  - Select the *manifest.json* file from this directory
 
 If the WebExtension was successfully installed, the following document should be rendered as a clickable HTML table:
 
@@ -13,6 +20,7 @@ For a general introduction to WebExtensions, see:
 
 https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Anatomy_of_a_WebExtension
 
+### Building the Project
 To build the project, run the following:
 
 ```sh
@@ -21,6 +29,5 @@ $ npm install
 $ npm run browserify
 ```
 
-Known issues:
- - no content negotiation yet (requests should be intercepted too)
- - when pages are fetched for the 2nd time, a cache is hit instead: the WebExtension script is never called
+## Known issues
+ - When pages are fetched for the 2nd time, a cache is hit instead: the WebExtension script is never called.
--- src/intercept.js
+++ src/intercept.js
@@ -1,10 +1,40 @@
 const formats = [
+    //'application/rdf+xml', /* currently no support */
     'application/ld+json',
     'text/turtle'
 ];
+const filter = {
+    urls: ["<all_urls>"]
+};
 
-function rewrite(details) {
-    let ct = details.responseHeaders.find(h => h.name.toLowerCase() == 'content-type');
+/**
+ * Change the accept header for all HTTP requests to include the content types specified in formats
+ * with higher priority than the remaining content types
+ * @param details The details of the HTTP request
+ * @returns {{requestHeaders: *}} The modified request header
+ */
+function changeHeader(details) {
+    for (let header of details.requestHeaders) {
+        if (header.name.toLowerCase() === "accept") {
+            let newHeader = "";
+            for (const f of formats)
+                 newHeader += f + ',';
+            newHeader = newHeader.substring(0, newHeader.length-1);
+            newHeader += ";q=0.95,";
+            header.value = newHeader + header.value;
+            break;
+        }
+    }
+    return {requestHeaders: details.requestHeaders};
+}
+
+/**
+ * Rewrite the payload of an HTTP response if format of content-type matches any in formats
+ * @param details The details of the HTTP response
+ * @returns {{}|{responseHeaders: {name: string, value: string}[]}} The modified response header
+ */
+function rewritePayload(details) {
+    let ct = details.responseHeaders.find(h => h.name.toLowerCase() === 'content-type');
     let format = ct ? formats.find(f => ct.value.includes(f)) : false;
 
     if (format) {
@@ -15,10 +45,10 @@ function rewrite(details) {
         filter.ondata = event => {
             let str = decoder.decode(event.data, {stream: true});
             renderer.render(str, format)
-                .then(html => {
-                    filter.write(encoder.encode(html));
-                    filter.close();
-                });
+              .then(html => {
+                  filter.write(encoder.encode(html));
+                  filter.close();
+              });
         };
 
         return {
@@ -31,8 +61,9 @@ function rewrite(details) {
     return {};
 }
 
-filter = {
-    urls: ["<all_urls>"]
-};
 
-browser.webRequest.onHeadersReceived.addListener(rewrite, filter, ["blocking", "responseHeaders"]);
+/**
+ * Add the listeners for modifying HTTP request and response headers as well as the response payload
+ */
+browser.webRequest.onBeforeSendHeaders.addListener(changeHeader, filter, ["blocking", "requestHeaders"]);
+browser.webRequest.onHeadersReceived.addListener(rewritePayload, filter, ["blocking", "responseHeaders"]);
